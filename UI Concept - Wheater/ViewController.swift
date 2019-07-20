f8a4d84f37cf3f0ea390c9baa73711ebfb54564d
Suprianto D <ersaka96@gmail.com>
2019-07-20 13:46:53 +0800

    Initial Commit

---
+++ UI Concept - Wheater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI Concept - Wheater
+//
+//  Created by Suprianto Djamalu on 20/07/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
